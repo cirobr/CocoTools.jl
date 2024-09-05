9eb48250e04247a1229b2565b5f2d2fc5dbf70b4
cirobr <ciro.rosa@alumni.usp.br>
2024-09-05 16:47:35 +0000

    wip

--- src/main.jl
+++ src/main.jl
@@ -52,8 +52,8 @@ function coco_download(folder::String)
     Threads.@sync begin
         # Threads.@spawn run(`unzip $(coco_data_folder)/train2017.zip -d $(coco_data_folder)`)
         # Threads.@spawn run(`unzip $(coco_data_folder)/val2017.zip -d $(coco_data_folder)`)
-        Threads.@spawn run(`unzip $(coco_data_folder)/annotations_trainval2017.zip -d $(coco_data_folder)`)
-        Threads.@spawn run(`unzip $(coco_data_folder)/stuffthingmaps_trainval2017.zip -d $(coco_data_folder)`)
+        # Threads.@spawn run(`unzip -o $(coco_data_folder)/annotations_trainval2017.zip -d $(coco_data_folder)`)
+        Threads.@spawn run(`unzip -o $(coco_data_folder)/stuffthingmaps_trainval2017.zip -d $(coco_data_folder)`)
     end
     @info "COCO data downloaded and unzipped."
 end
