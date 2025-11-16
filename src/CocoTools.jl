7df5a5f1d7088c26b3446e52fe581ace97d7e988
cirobr <ciro.rosa@alumni.usp.br>
2025-11-16 09:59:06 -0300

    upd Colors

--- src/CocoTools.jl
+++ src/CocoTools.jl
@@ -6,7 +6,7 @@ export dfcoco, coco_rgb2classes, coco_download
 import JSON3, HTTP
 import DataFrames: DataFrame
 import DataStructures
-import Colors: RGB
+import Colors: RGB, N0f8
 import Downloads
 
 include("./main.jl")
