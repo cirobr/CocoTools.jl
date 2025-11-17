01da425ca56b00ffdfbfbe9956e75489bfe8a3c7
cirobr <ciro.rosa@alumni.usp.br>
2025-11-17 10:43:12 -0300

    fix channelview

--- src/CocoTools.jl
+++ src/CocoTools.jl
@@ -6,7 +6,7 @@ export dfcoco, coco_rgb2classes, coco_download
 import JSON3, HTTP
 import DataFrames: DataFrame
 import DataStructures
-import Colors: RGB, N0f8
+import Colors: RGB, N0f8, channelview
 import Downloads
 
 include("./main.jl")
